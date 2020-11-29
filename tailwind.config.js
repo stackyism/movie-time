d901a256a5bde72b3449cfaa6d436b8d2feb651c
Saagar Takhi <saagar.takhi@sprinklr.com>
2020-11-29 15:40:20 +0530

    feature(movie-house): styles added

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors : {
+        clr01 : "#38a9c7",
+        clr02 : "#131a20",
+      }
+    },
+  },
+  variants: {
+    extend: {
+    },
+  },
+  plugins: [],
+}
